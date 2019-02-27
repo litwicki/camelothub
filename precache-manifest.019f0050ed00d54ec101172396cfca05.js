cc88709480f857e310aad7382dfd5bedb1c9726d
Jake Litwicki <jake.litwicki@gmail.com>
2019-02-27 13:30:32 -0800

    Updates

---
+++ precache-manifest.019f0050ed00d54ec101172396cfca05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b22f8351482be35b66a",
+    "url": "/material-sense/static/css/main.9738f725.chunk.css"
+  },
+  {
+    "revision": "2b22f8351482be35b66a",
+    "url": "/material-sense/static/js/main.2b22f835.chunk.js"
+  },
+  {
+    "revision": "ca1c37c262c2cbad1b6f",
+    "url": "/material-sense/static/js/1.ca1c37c2.chunk.js"
+  },
+  {
+    "revision": "09f4e86c218a577ad5da",
+    "url": "/material-sense/static/js/runtime~main.09f4e86c.js"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/material-sense/static/media/shape.208f3d38.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material-sense/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "700e2c43c648fddb5bd9db43f75ba60c",
+    "url": "/material-sense/index.html"
+  }
+];
