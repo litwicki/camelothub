c4c90cb2d09c6b1c66a45a2799e545b054494e1d
Jake Litwicki <jakelitwicki@gmail.com>
2019-03-05 16:14:19 -0800

    reset repo, setup navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
+import NavBar from './components/layout/NavBar'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src="https://camelotunchained.com/v3/wp-content/themes/CSE%20v2/images/cu_logo_game.png" alt="logo" />
-        </header>
+      <div>
+        <NavBar />
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
