3d6a735537e958136b0299d8ab7ccc17b69c0859
Jake Litwicki <jake.litwicki@gmail.com>
2019-03-18 10:57:45 -0700

    remove config file from repo

--- app/config/auth0.js
+++
@@ -1,7 +0,0 @@
-export const AUTH_CONFIG = {
-  AUTH0_DOMAIN: 'camelothub.auth0.com',
-  AUTH0_CLIENT_ID: 'e33QAyM4HuLn1rc4xGdsRQgUqKjwYEnA',
-  AUTH0_CALLBACK_URL: 'http://localhost:3000/callback',
-  AUTH0_SCOPE: 'openid',
-  AUTH0_RESPONSE_TYPE: 'token id_token',
-};
